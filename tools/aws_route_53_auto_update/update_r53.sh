9a470646a1cecce3f063091d0fae2f9ae63a8cb6
Ammar Bombaywala <ammarbombaywala1@gmail.com>
2022-10-27 06:25:34 +0000

    aws_route_53_auto_update

---
+++ tools/aws_route_53_auto_update/update_r53.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Tested on Ubuntu ARM EC2 Instance
+# To Create Policy and EC2 Role follow: https://dev.to/aws/amazon-route-53-how-to-automatically-update-ip-addresses-without-using-elastic-ips-h7o
+# OR
+# Create a Policy to allow editing a particular hosted Zone
+# Create EC2 Role and assign it that Policy
+# Assign that role to your instance
+# Preriquisites
+# Install aws cli
+#sudo apt intall awscli -y
+# Make Script executable and copy to /var/lib/cloud/scripts/per-boot
+#sudo chmod +x update_r53.sh
+
+# Extract information about the Instance
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id/)
+AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/)
+MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4/)
+
+# Extract tags associated with instance
+#ZONE_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_ZONE`].Value' --output text)
+#NAME_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_NAME`].Value' --output text)
+ZONE_TAG=<AWS_Hosted_Zone_ARN>
+NAME_TAG=<domain.hostname.example.com>
+
+# Update Route 53 Record Set based on the Name tag to the current Public IP address of the Instance
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_TAG --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$NAME_TAG'","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$MY_IP'"}]}}]}'
