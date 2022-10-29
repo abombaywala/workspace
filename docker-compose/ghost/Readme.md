987b95dc2a08b7d323e6472403f27290cd3df5d9
Ammar Bombaywala <ammarbombaywala1@gmail.com>
2022-10-29 18:37:26 +0000

    added docker compose file

---
+++ docker-compose/ghost/Readme.md
@@ -0,0 +1,13 @@
+# User data to install docker and Docker compose
+# chect at boot by running
+# docker info
+# docker-compose version
+
+#!/bin/bash
+yum update -y
+amazon-linux-extras install docker
+service docker start
+systemctl enable docker
+usermod -a -G docker ec2-user
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
--- tools/aws_route_53_auto_update/update_r53.sh
+++ tools/aws_route_53_auto_update/update_r53.sh
@@ -18,10 +18,8 @@ AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone
 MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4/)
 
 # Extract tags associated with instance
-#ZONE_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_ZONE`].Value' --output text)
-#NAME_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_NAME`].Value' --output text)
-ZONE_TAG=<AWS_Hosted_Zone_ARN>
-NAME_TAG=<domain.hostname.example.com>
+ZONE_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_ZONE`].Value' --output text)
+NAME_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_NAME`].Value' --output text)
 
 # Update Route 53 Record Set based on the Name tag to the current Public IP address of the Instance
 aws route53 change-resource-record-sets --hosted-zone-id $ZONE_TAG --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$NAME_TAG'","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$MY_IP'"}]}}]}'
